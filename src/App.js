861a5a181c7557cb1bc4b873988295009a7090a7
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-09-10 00:18:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Courses from './pages/Courses';
-import About from './pages/About';
-import Contact from './pages/Contact';
-
-import WhyAnzWay from './pages/WhyAnzWay';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
-      
       {/* Navbar with white background */}
-      <div className="bg-white text-primary">
+      <div className="bg-white text-primary shadow-md">
         <Navbar />
       </div>
 
-
-      {/* Main content area with light purple background */}
-      <div className="bg-lightPurple min-h-screen">
+      {/* Main content area */}
+      <div className="bg-gray-50 min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<Courses />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/Why_Anz_Way" element={<WhyAnzWay />} />
         </Routes>
       </div>
 
-      {/* Footer with dark purple background */}
-      <div className="bg-secondary text-white">
+      {/* Footer */}
+      <div className="bg-green-800 text-white">
         <Footer />
       </div>
     </Router>
