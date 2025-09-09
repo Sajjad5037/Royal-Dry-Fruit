0e2f5a89dbb9a0915bfa58a5499f81b0b1f4c6ff
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-09-10 01:47:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import ShopOnline from "./pages/ShopOnline";
 
 function App() {
   
