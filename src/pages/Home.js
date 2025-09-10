070808eb6bdedd38936e393861fbc9ec1979d83e
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-09-10 18:21:33 +0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
 
       {/* Banner */}
       <img
-        src="/images/cover_photo.jpeg"
+        src="/images/cover_photo.jpg"
         alt="Royal Dry Fruits Banner"
         className="w-full h-72 object-cover"
       />
